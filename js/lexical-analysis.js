b6d79f3b17b3361ef109509737cddb900940ef9f
Marcos Lima <marcoslima.xv@gmail.com>
2017-11-08 10:30:49 -0200

    gerate table and states

--- js/lexical-analysis.js
+++ js/lexical-analysis.js
@@ -38,7 +38,7 @@ $( "#form-token" ).submit(function( event ) {
 
     for (j = 0; j < iterator_tokens.length; j++) {
       //var teste = states[initial_state][iterator_tokens];
-      var type = typeof states[current][iterator_tokens];
+      var type = typeof states[current][iterator_tokens[j]];
       // console.log(type);
       if (type === typeof undefined){
         //console.log('Is a undefined')
@@ -47,17 +47,85 @@ $( "#form-token" ).submit(function( event ) {
         states[current][iterator_tokens[j]] = next;
         //console.log(states);
         states[next] = [];
-        //console.log(states);
-        overview = current = next;
-
+        //console.log(states[next]);
+        current = next;
+        overview = current;
       } else {
-        //console.log('defined');
+        console.log('defined');
         current = states[current][iterator_tokens[j]];
       }
       if (j == iterator_tokens.length -1) {
         states[current]['terminal'] = true;
       }
     }
-    console.log(states); //-> states debug  
+    console.log(states); //-> states debug
+  }
+
+  var array_tmp = [];
+  for (i = 0; i < states.length; i++) {
+      var swap = [];
+      swap['states'] = i;
+
+      var first = 'a';
+      var last = 'z';
+      for (j = first.charCodeAt(0); j <= last.charCodeAt(0); j++ ) {
+        var symbol = String.fromCharCode(j);
+        if (typeof states[i][symbol] === 'undefined') {
+          swap[symbol] = '-';
+        } else {
+          swap[symbol] = states[i][symbol];
+        }
+      }
+      if (typeof states[i]['terminal'] !== 'undefined') {
+        swap['terminal'] = true;
+      }
+      array_tmp.push(swap);
+  }
+  console.log(array_tmp);
+
+  /*   Tabela   */
+  var html_table = $('table');
+  html_table.html('');
+
+  var html_tr = $(document.createElement('tr'));
+  var html_th = $(document.createElement('th'));
+
+  html_th.html('Estado');
+  html_tr.append(html_th);
+
+  // console.log(first);
+  // console.log(last);
+
+  for (i = first.charCodeAt(0); i <= last.charCodeAt(0); i++) {
+    var html_th = $(document.createElement('th'));
+    html_th.html(String.fromCharCode(i));
+    html_tr.append(html_th);
+  }
+  html_table.append(html_tr);
+
+  for (i = 0; i < array_tmp.length; i++) {
+    var html_tr = $(document.createElement('tr'));
+    var html_td = $(document.createElement('td'));
+
+    if (array_tmp[i]['terminal']) {
+      html_td.html('q' + array_tmp[i]['states'] + '*');
+    } else {
+      html_td.html('q' + array_tmp[i]['states']);
+    }
+    html_tr.append(html_td);
+    html_tr.addClass('states_'+array_tmp[i]['states']);
+
+    for (j = first.charCodeAt(0); j <= last.charCodeAt(0); j++) {
+      var symbol = String.fromCharCode(j);
+
+      var html_td = $(document.createElement('td'));
+      html_td.addClass('word-'+symbol);
+
+      if (array_tmp[i][symbol] != '-') {
+        html_td.html('q' + array_tmp[i][symbol]);
+      }
+      html_tr.append(html_td);
+    }
+    html_table.append(html_tr);
   }
 });
